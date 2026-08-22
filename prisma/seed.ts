1eefe2eaa73de36e7a12111be90b1111a414f3c2
maklersizkvartira <maklersizkvartira@gmail.com>
2026-08-22 15:04:09 +0500

    Add PrismaPg adapter for Prisma v7 PostgreSQL support

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,13 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 import bcrypt from 'bcrypt'
 
-const prisma = new PrismaClient()
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+const prisma = new PrismaClient({ adapter })
 
 async function main() {
   const passwordHash = await bcrypt.hash('123456', 10)
